8321a1bfd1e7c04f5bb6153c1774a5212c12b899
den <kickrop@gmail.com>
2019-03-25 07:52:37 +0300

    buy button

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -40,7 +40,8 @@ export default {
                 stockPrice: this.stock.price,
                 quantity: this.quantity
             };
-            console.log(order);
+            // console.log(order);
+            this.$store.dispatch('buyStock', order);
             this.quantity = 0;
         }
     }
