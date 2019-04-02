f390b50e7d67497be9589f2a2751cf065b51ba38
den <kickrop@gmail.com>
2019-04-02 07:11:50 +0300

    added vue resource

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 
 import App from './App.vue'
 import { routes } from './routes';
 import store from './store/store';
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+vue.http.options.root = "https://vuejs-stock-trader-f3e0a.firebaseio.com/";
 
 Vue.filter('currency', (value) => {
   return "$" + value.toLocaleString();
