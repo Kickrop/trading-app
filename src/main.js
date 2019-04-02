a09c47d4f5f98e1d243c32a0953efb576919b7f6
den <kickrop@gmail.com>
2019-04-02 07:24:37 +0300

    saving to fireBase added

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import store from './store/store';
 Vue.use(VueRouter);
 Vue.use(VueResource);
 
-vue.http.options.root = "https://vuejs-stock-trader-f3e0a.firebaseio.com/";
+Vue.http.options.root = "https://vuejs-stock-trader-f3e0a.firebaseio.com/";
 
 Vue.filter('currency', (value) => {
   return "$" + value.toLocaleString();
